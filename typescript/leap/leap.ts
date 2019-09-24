fb6f2de542169cb15b90b04d79a2b78769d99ddd
Andrew Deal <deal.e.andrew@gmail.com>
2019-09-25 02:06:47 +0300

    Solution for leap

---
+++ typescript/leap/leap.ts
@@ -0,0 +1,8 @@
+function isLeapYear(year: number): boolean {
+  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+    return true;
+  }
+  return false;
+}
+
+export default isLeapYear;
