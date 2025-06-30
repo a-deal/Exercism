0b9440e7c513037ce66dd42be0de5fc55bdddf32
Andrew Deal <deal.e.andrew@gmail.com>
2025-06-30 13:15:38 -0500

    Add solution for Resistor Color Duo

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,28 +1,54 @@
-enum Colors {
-  Black,
-  Brown,
-  Red,
-  Orange,
-  Yellow,
-  Green,
-  Blue,
-  Violet,
-  Grey,
-  White
+enum ResistanceColors {
+  Black = 0,
+  Brown = 1,
+  Red = 2,
+  Orange = 3,
+  Yellow = 4,
+  Green = 5,
+  Blue = 6,
+  Violet = 7,
+  Grey = 8,
+  White = 9,
 }
-export class ResistorColor {
-  private colors: string[]
 
-  constructor(colors: string[]) {
-    if (colors.length < 2) throw 'At least two colors need to be present'
-    this.colors = colors
-  }
-  value = ():number => {
-    let val = ''
-    for (let i = 0; i < 2; i++){
-      const resistorColorName = this.colors[i].charAt(0).toUpperCase() + this.colors[i].slice(1)
-      val += Colors[resistorColorName as keyof typeof Colors]
+function capitalizeColor(color: string): string {
+  return color.charAt(0).toUpperCase() + color.slice(1);
+}
+
+function isRecognizedColor(color: string): boolean {
+  const colorKey = capitalizeColor(color)
+  return Object.prototype.hasOwnProperty.call(ResistanceColors, colorKey)
+}
+
+function convertColorToNumber(color: string): number {
+  const colorKey = capitalizeColor(color)
+
+  return ResistanceColors[colorKey as keyof typeof ResistanceColors]
+}
+
+export function decodedValue(colors: string[]): number {
+  try {
+    const [firstColor, secondColor] = colors
+
+    if (!isRecognizedColor(firstColor)) {
+      throw new Error(`Invalid color argument for decodedValue: ${firstColor}`);
     }
-    return parseInt(val)
+
+    const firstColorValue: number = convertColorToNumber(firstColor)
+
+    if (!isRecognizedColor(secondColor)) {
+      return firstColorValue
+    }
+    const secondColorValue: number = convertColorToNumber(secondColor)
+
+    return firstColorValue * 10 + secondColorValue
+
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(`Error decoding color value: ${error.message}`);
+    } else {
+      console.error('Unknown error occurred');
+    }
+    return -1; // Return -1 to indicate an error occurred
   }
 }
